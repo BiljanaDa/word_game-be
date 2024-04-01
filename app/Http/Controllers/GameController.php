91b90d1675456642d01a8930ea48fdea58e1980d
BiljanaDa <bibidakovic@gmail.com>
2024-04-01 22:21:25 +0200

    game controller, functionality, api route

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class GameController extends Controller
+{
+    public function score(Request $request) {
+
+        $word = strtolower($request->input('word'));
+        if(!$this->dictionary($word)) {
+            return response()->json(['error' => 'Word not found in dictionary']);
+        }
+
+        $score =  count(array_unique(str_split($word)));
+        if($this->isPalindrome($word)) {
+            $score += 3;
+        } elseif($this->isPalindrome($word)) {
+            $score += 2;
+        }
+
+        return response()->json(['score' => $score]);
+
+    }
+
+    private function dictionary($word) {
+        $response = Http::get("https://api.dictionaryapi.dev/api/v2/entries/en/$word");
+        return $response->ok();
+    }
+
+    private function isPalindrome($word) {
+        return $word === strrev($word);
+    }
+
+    private function isAlmostPalindrome($word) {
+        $length = strlen($word);
+
+        for($i = 0; $i < $length/2; $i++) {
+            if($word[$i] !== $word[$length - 1 - $i]) {
+                $withoutChar = substr($word, 0, $i) . substr($word, $i + 1);
+                if($this->isPalindrome($withoutChar)) {
+                    return true;
+                }
+                return false; 
+            }
+        }
+        return true;
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\GameController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/word', GameController::class, 'score');
