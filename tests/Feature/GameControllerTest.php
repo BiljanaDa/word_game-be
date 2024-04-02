b1714049b7be801b868bd63fec659e049f53c744
BiljanaDa <bibidakovic@gmail.com>
2024-04-02 21:28:11 +0200

    functional tests

--- tests/Feature/GameControllerTest.php
+++ tests/Feature/GameControllerTest.php
@@ -9,14 +9,43 @@ use Tests\TestCase;
 class GameControllerTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Test scoring for a normal word
      */
-    public function test_example(): void
+  
+    public function testNormalwordScoring(): void 
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response = $this->postJson('/api/word', ['word' => 'hello']);
+        $response->assertStatus(200)->assertJson(['score' => 4]);
     }
 
+     /**
+     * Test scoring for a palindrome word
+     */
+
+     public function testPalindromeWordScoring(): void 
+     {
+        $response = $this->postJson('/api/word', ['word' => 'level']);
+        $response->assertStatus(200)->assertJson(['score' => 6]);
+     }
+
+      /**
+     * Test scoring for an almost palindrome word
+     */
+
+     public function testAlmostPalindromeWordScoring(): void 
+     {
+        $response = $this->postJson('/api/word', ['word' => 'engage']);
+        $response->assertStatus(200)->assertJson(['score' => 6]);
+     }
+
+      /**
+     * Test scoring for a word not found in dictionary
+     */
+
+     public function testWordNotInDictionary(): void 
+     {
+        $response = $this->postJson('/api/word', ['word' => 'wqwqwqw']);
+        $response->assertStatus(200)->assertJson(['error' => 'Word not found in dictionary']);
+     }
     
 }
